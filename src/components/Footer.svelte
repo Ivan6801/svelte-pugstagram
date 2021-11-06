1bf026e8fac6d189a30f8c6a16b6036ef401f11f
Ivan Gonzalez <hal308366@gmail.com>
2021-11-06 13:20:01 -0700

    svelte

---
+++ src/components/Footer.svelte
@@ -0,0 +1,16 @@
+<footer class="Footer">
+    <div class="Footer-container">
+      <div class="Footer-text">
+        ©️ 2021 PUGSTAGRAM
+      </div>
+    </div>
+  </footer>
+
+  <style>
+    .Footer {
+      font-size: 11px;
+      letter-spacing: 1px;
+      font-weight: normal;
+      color: #c7c7c7;
+    }
+  </style>
---
+++ src/components/Profile.svelte
@@ -0,0 +1,40 @@
+<script>
+  export let Name = 'IvanGlz';
+</script>
+
+<div class="Profile">
+    <div class="Profile-content">
+        <div class="Profile-avatar">
+            <img src="https://avatars.githubusercontent.com/u/60527338?v=4" alt="">
+        </div>
+        <div class="Profile-info">
+            <h2>{Name}</h2>
+            <span>{Name}</span>
+        </div>
+    </div>
+</div>
+
+<style>
+    .Profile-content {
+      display: flex;
+      align-items: center;
+    }
+    .Profile-avatar img {
+      width: 42px;
+      height: 42px;
+      border-radius: 50%;
+    }
+    .Profile-info {
+      margin: 0 0 0 0.5em;
+    }
+    .Profile-info h2 {
+      font-size: 14px;
+      color: black;
+      margin: 0;
+      padding: 0;
+    }
+    .Profile-info span {
+      font-size: 12px;
+      font-weight: normal;
+    }
+  </style>
